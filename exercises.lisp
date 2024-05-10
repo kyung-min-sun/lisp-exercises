47f8f44b435de512f8ac325d1b6694f6124c50f2
kevinsun127 <kevinsun@usc.edu>
2024-05-10 07:14:41 -0700

    init

---
+++ exercises.lisp
@@ -0,0 +1,14 @@
+(defun test (x) (* x 2))
+(mapcar #'(lambda (x) (+ x 10)) '(1 2 3))
+;;; how would you implement a hashmap in lisp?
+;;; how would you implement an O(1) access / push array in lisp? 
+;;; that's pretty easy actually (use `nth x '(1, 2, 3)`)
+(defun range (max) 
+  (let ((range-array (make-array max))) 
+  (progn
+    (dotimes (i max)
+      (setf (aref range-array i) i))
+    range-array
+  )))
+
+(defun time-array-access (n) (let ((x (range n))) (time (aref x (- n 1)))))
